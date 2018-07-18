643ee87bb86727df26ce6b31bbc82866be63cf73
ryanlee201 <ryanlee391@gmail.com>
2018-07-17 20:55:41 -0400

    adding dnd project

---
+++ app/src/main/java/com/example/dndapp/SlidingImage_Adapter.java
@@ -0,0 +1,79 @@
+package com.example.dndapp;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.example.myfirstapp.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SlidingImage_Adapter extends PagerAdapter{
+    Context context;
+    int images[];
+    LayoutInflater layoutInflater;
+
+
+    public SlidingImage_Adapter(Context context, String[] imageNames) {
+        this.context = context;
+        this.images = imageNamesToIds(imageNames);
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((RelativeLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+        View itemView = layoutInflater.inflate(R.layout.spellcard_view, container, false);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.SpellCard);
+        imageView.setImageResource(images[position]);
+
+        container.addView(itemView);
+
+        //listening to image click
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "you clicked image " + (position + 1), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((RelativeLayout) object);
+    }
+
+    public int[] imageNamesToIds(String[] imageNames)
+    {
+        int[] imageIds= new int[imageNames.length];
+        for(int i = 0; i < imageIds.length; i++)
+        {
+            int resId = context.getResources().getIdentifier(imageNames[i],"drawable",context.getPackageName());
+            imageIds[i] = resId;
+
+        }
+        //Log.d("this is my array", "String: " + Arrays.toString(imageNames));
+        //Log.d("this is my array", "Integer: " + Arrays.toString(imageIds));
+        return imageIds;
+    }
+}
---
+++ app/src/main/java/com/example/dndapp/ViewSpellCard.java
@@ -0,0 +1,59 @@
+package com.example.dndapp;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.myfirstapp.R;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ViewSpellCard extends AppCompatActivity {
+    ViewPager viewPager;
+    int images[] = {R.drawable.guidance, R.drawable.light, R.drawable.mending};
+    SlidingImage_Adapter myCustomPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spellcard_slider);
+
+        viewPager = (ViewPager)findViewById(R.id.pager);
+
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+
+        Bundle extras = getIntent().getExtras();
+
+        int [] filterLvls = new int[10];
+        String[] filterClasses = new String[12];
+
+        if (extras != null) {
+             filterLvls = Arrays.copyOf(extras.getIntArray("Spell Level"),10);
+             filterClasses = Arrays.copyOf(extras.getStringArray("Spell Class"),12);
+        }
+        else
+        {
+            myCustomPagerAdapter = new SlidingImage_Adapter(ViewSpellCard.this, databaseAccess.getSpellCards());
+            viewPager.setAdapter(myCustomPagerAdapter);
+            databaseAccess.close();
+        }
+
+
+
+//        int[] = {}
+//        databaseAccess.filterByClass(filterClasses);
+
+
+        myCustomPagerAdapter = new SlidingImage_Adapter(ViewSpellCard.this, databaseAccess.filterByLevel(filterLvls));
+        viewPager.setAdapter(myCustomPagerAdapter);
+        databaseAccess.close();
+    }
+}
+
